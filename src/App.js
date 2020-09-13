e895d10aac68ad6164c819b7a4a57a2d86227e54
Jade Del Rosario-Bell <JadeDRB@Jades-MBP.lan1>
2020-09-13 16:58:10 -0400

    connecting frontend to backend

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      employees: []
+      employees: [],
+      loading: false,
+      interval: false,
+      dots: ''
     }
   }
 
@@ -19,7 +22,30 @@ class App extends Component {
     this.getEmployees() 
   }
 
+  loadingFunc = () => {
+
+    let dotsInterval = false
+
+    if (!this.state.loading) {
+      dotsInterval = setInterval(() => {
+        if (this.state.dots.length < 3) {
+          this.setState(state => {
+            return {
+              dots: state.dots + '.'
+            }
+          })
+        } 
+        else this.setState({ dots: '' })
+        console.log('interval firing...')
+      }, 100)
+    } else clearInterval(this.state.interval)
+
+    this.setState({ loading: !this.state.loading, interval: dotsInterval })
+
+  }
+
   getEmployees = async () => {
+    this.loadingFunc()
     try {
       let response = await axios.get('/api/employees')
       this.setState({ employees: response.data })
@@ -28,9 +54,16 @@ class App extends Component {
     catch (err) {
       console.log('error occured')
     }
+    finally {
+      console.log('finally')
+      console.log(this.state)
+      this.loadingFunc()
+    }
   }
 
   deleteEmployee = async (id) => {
+    console.log('delete')
+    this.loadingFunc()
     try {
       let response = await axios.delete(`/api/employees/${id}`)
       this.getEmployees()
@@ -40,33 +73,51 @@ class App extends Component {
       console.log('error occured')
       console.log(err)
     }
+    finally {
+      this.loadingFunc()
+    }
   }
 
   updateEmployee = async (employee, id) => {
     console.log('update')
+    this.loadingFunc()
     try {
       let response = await axios.put(`/api/employees/${id}`, employee)
       console.log(response)
       this.getEmployees()
-    } catch (err) {
+    } 
+    catch (err) {
       console.log('failed update')
+    }
+    finally {
+      this.loadingFunc()
     }
   }
 
   addNewEmployee = async (employee) => {
     console.log('create')
+    this.loadingFunc()
     try {
       let response = await axios.post(`/api/employees/`, employee)
       console.log(response)
       this.getEmployees()
-    } catch (err) {
+    } 
+    catch (err) {
       console.log('failed create')
+    }
+    finally {
+      this.loadingFunc()
     }
   }
 
   render() {
+
+    let { loading, dots } = this.state 
+    let loadStyle = loading ? { display: 'block' } : { display: 'none' }
+
     return (
       <>
+        <div className="loading dissappear" style={loadStyle}>loading{dots}</div>
         <Router>
           <Switch>
             <Route path='/employees' render={() => <CreateUpdateEmp 
