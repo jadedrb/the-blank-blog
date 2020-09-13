a71588042efa5106cafa1f64a6fc40c86f603f4c
Jade Del Rosario-Bell <JadeDRB@Jades-MBP.lan1>
2020-09-13 13:28:15 -0400

    first commit test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import CreateUpdateEmp from './CreateUpdateEmp';
+import Employees from './Employees';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      employees: []
+    }
+  }
+
+  componentDidMount() { 
+    // This app runs with the Employee2020 Java/Spring Boot server
+    this.getEmployees() 
+  }
+
+  getEmployees = async () => {
+    try {
+      let response = await axios.get('/api/employees')
+      this.setState({ employees: response.data })
+      console.log(response.data)
+    }
+    catch (err) {
+      console.log('error occured')
+      console.log(err)
+    }
+  }
+
+  deleteEmployee = async (id) => {
+    try {
+      let response = await axios.delete(`/api/employees/${id}`)
+      this.getEmployees()
+      console.log(response)
+    }
+    catch (err) {
+      console.log('error occured')
+      console.log(err)
+    }
+  }
+
+  updateEmployee = async (employee, id) => {
+    console.log('update')
+    try {
+      let response = await axios.put(`/api/employees/${id}`, employee)
+      console.log(response)
+      this.getEmployees()
+    } catch (err) {
+      console.log('failed update')
+    }
+  }
+
+  addNewEmployee = async (employee) => {
+    console.log('create')
+    try {
+      let response = await axios.post(`http://localhost:8080/company/api/v1/employees/`, employee)
+      console.log(response)
+      this.getEmployees()
+    } catch (err) {
+      console.log('failed create')
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Router>
+          <Switch>
+            <Route exact path='/' render={() => <Employees 
+                                                    employees={this.state.employees} 
+                                                    deleteEmployee={this.deleteEmployee}/>} />
+            <Route path='/employees' render={() => <CreateUpdateEmp 
+                                                      updateEmployee={this.updateEmployee}
+                                                      addNewEmployee={this.addNewEmployee}/>} />
+          </Switch>
+        </Router>
+      </>
+     );
+  }
 }
 
 export default App;
---
+++ src/CreateUpdateEmp.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation, useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const CreateUpdateEmp = (props) => {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [jobTitle, setJobTitle] = useState('')
+    const location = useLocation()
+    const history = useHistory()
+
+    let pathEnd = location.pathname[location.pathname.length - 1]
+    let creation = isNaN(pathEnd) ? true : false
+
+    let header = creation ? 'Add a New Employee' : `Update Details for Employee #${pathEnd}`
+
+    useEffect(() => {
+        if (!creation) getEmployeeById(pathEnd)
+    }, [])
+
+    const getEmployeeById = async (id) => {
+        console.log(id)
+        try {
+            let response = await axios.get(`/api/employees/${id}`)
+
+            // This mapping is to turn any 'null' properties into empty strings (because React)
+            Object.keys(response.data).map(property => !response.data[property] ? response.data[property] = '' : response.data[property])
+
+            let { firstName, lastName, email, jobTitle } = response.data
+
+            setFirstName(firstName)
+            setLastName(lastName)
+            setEmail(email)
+            setJobTitle(jobTitle)
+            console.log(response.data)
+        } catch (err) {
+
+        }
+    }
+
+    const handleChange = e => {
+    
+        let { id, value } = e.target
+
+        switch (id) {
+            case 'first':
+                setFirstName(value)
+                break;
+            case 'last':
+                setLastName(value)
+                break;
+            case 'email':
+                setEmail(value)
+                break;
+            case 'title':
+                setJobTitle(value)
+            default:
+                break;
+        }
+
+        console.log(id)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        let employeeObj = {
+            firstName,
+            lastName,
+            email,
+            jobTitle
+        }
+
+        creation ? props.addNewEmployee(employeeObj) : props.updateEmployee(employeeObj, pathEnd)
+        history.push('/')
+    }
+
+    return (
+        <>
+            <Link className='link' to='/'>Back</Link>
+            <h1>{header}</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="first">First Name</label>
+                <input id="first" value={firstName} onChange={handleChange} />
+
+                <label htmlFor="last">Last Name</label>
+                <input id="last" value={lastName} onChange={handleChange}/>
+
+                <label htmlFor="email">Email</label>
+                <input id="email" value={email} onChange={handleChange}/>
+
+                <label htmlFor="title">Title</label>
+                <input id="title" value={jobTitle} onChange={handleChange}/>
+
+                <button>{creation ? 'ADD' : 'UPDATE'}</button>
+            </form>
+        </>
+    )
+}
+
+export default CreateUpdateEmp;
