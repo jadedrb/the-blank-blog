8a27909118e583dea5aa5205147a4375c331e177
Jade <jadecdrb@gmail.com>
2021-11-01 09:44:12 -0400

    Updates

---
+++ precache-manifest.ba65f04a04dffe95b96d23c2874c624d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f754b3d30a7288b88361ee0bb6c4945",
+    "url": "/the-blank-blog/index.html"
+  },
+  {
+    "revision": "07a62944c1ebb6fca350",
+    "url": "/the-blank-blog/static/css/main.d8e57888.chunk.css"
+  },
+  {
+    "revision": "d34a6db46e813b738dee",
+    "url": "/the-blank-blog/static/js/2.d5d7cd9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-blank-blog/static/js/2.d5d7cd9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07a62944c1ebb6fca350",
+    "url": "/the-blank-blog/static/js/main.35bf96e1.chunk.js"
+  },
+  {
+    "revision": "61a49ded7552b137b6f6",
+    "url": "/the-blank-blog/static/js/runtime-main.65f15499.js"
+  }
+]);
